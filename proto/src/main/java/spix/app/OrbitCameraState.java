fa68e0e02ba4b49131daab25d07dac4081aa67b2
Paul Speed <pspeed42@users.noreply.github.com>
2016-03-19 03:10:58 -0400

    A partially working orbit state.  The math is bad but the input mechanics and focal point tracking are right.

--- proto/src/main/java/spix/app/OrbitCameraState.java
+++ proto/src/main/java/spix/app/OrbitCameraState.java
@@ -38,11 +38,14 @@ package spix.app;
 
 import com.jme3.app.*;
 import com.jme3.app.state.*;
+import com.jme3.app.state.BaseAppState;
 import com.jme3.input.KeyInput;
 import com.jme3.math.*;
 import com.jme3.renderer.Camera;
+import com.jme3.scene.*;
 
 import com.simsilica.lemur.GuiGlobals;
+import com.simsilica.lemur.event.*;
 import com.simsilica.lemur.input.*;
 
 /**
@@ -54,7 +57,8 @@ public class OrbitCameraState extends BaseAppState {
  
     public static final String GROUP = "Orbit Camera";
     public static final FunctionId F_ORBIT_X = new FunctionId(GROUP, "Orbit X"); 
-    public static final FunctionId F_ORBIT_Y = new FunctionId(GROUP, "Orbit Y"); 
+    public static final FunctionId F_ORBIT_Y = new FunctionId(GROUP, "Orbit Y");
+    public static final FunctionId F_ORBIT_ON = new FunctionId(GROUP, "Orbit On"); 
     public static final FunctionId F_MOVE = new FunctionId(GROUP, "Move"); 
     public static final FunctionId F_STRAFE = new FunctionId(GROUP, "Strafe"); 
     public static final FunctionId F_ELEVATION = new FunctionId(GROUP, "Elevation"); 
@@ -62,9 +66,13 @@ public class OrbitCameraState extends BaseAppState {
 
     private Camera cam;
     private InputHandler inputHandler = new InputHandler();
+    private FocalPointListener focalPointListener = new FocalPointListener();
     private float normalSpeed = 5;
     private float fastSpeed = 20;
-    private float speed = normalSpeed; 
+    private float speed = normalSpeed;
+    
+    private Vector3f focalPoint = null;
+    private Vector3f calculatedFocalPoint = new Vector3f(); 
     
     public OrbitCameraState() {
         this(true);
@@ -91,16 +99,18 @@ public class OrbitCameraState extends BaseAppState {
     
         InputMapper inputMapper = GuiGlobals.getInstance().getInputMapper();
         inputMapper.addAnalogListener(inputHandler, F_ORBIT_X, F_ORBIT_Y, F_MOVE, F_STRAFE, F_ELEVATION);
-        inputMapper.addStateListener(inputHandler, F_FAST);
+        inputMapper.addStateListener(inputHandler, F_FAST, F_ORBIT_ON);
         
         // See if there are already mappings for these functions.
         if( !inputMapper.hasMappings(F_ORBIT_X) ) {
             System.out.println("Initializing default mappings for:" + F_ORBIT_X);
             inputMapper.map(F_ORBIT_X, Axis.MOUSE_X, Button.MOUSE_BUTTON2); 
+            inputMapper.map(F_ORBIT_ON, Button.MOUSE_BUTTON2); 
         }
         if( !inputMapper.hasMappings(F_ORBIT_Y) ) {
             System.out.println("Initializing default mappings for:" + F_ORBIT_Y);
             inputMapper.map(F_ORBIT_Y, Axis.MOUSE_Y, Button.MOUSE_BUTTON2); 
+            inputMapper.map(F_ORBIT_ON, Button.MOUSE_BUTTON2); 
         }
         if( !inputMapper.hasMappings(F_MOVE) ) {
             System.out.println("Initializing default mappings for:" + F_MOVE);
@@ -129,7 +139,7 @@ public class OrbitCameraState extends BaseAppState {
     protected void cleanup( Application app ) {
         InputMapper inputMapper = GuiGlobals.getInstance().getInputMapper();
         inputMapper.removeAnalogListener(inputHandler, F_ORBIT_X, F_ORBIT_Y, F_MOVE, F_STRAFE, F_ELEVATION);
-        inputMapper.removeStateListener(inputHandler, F_FAST);
+        inputMapper.removeStateListener(inputHandler, F_FAST, F_ORBIT_ON);
     }
     
     @Override   
@@ -137,6 +147,9 @@ public class OrbitCameraState extends BaseAppState {
         System.out.println(getClass().getName() + " Enabled");
         InputMapper inputMapper = GuiGlobals.getInstance().getInputMapper();
         inputMapper.activateGroup(GROUP);
+ 
+        Node rootNode = ((SimpleApplication)getApplication()).getRootNode();       
+        CursorEventControl.addListenersToSpatial(rootNode, focalPointListener);
     }
     
     @Override   
@@ -144,9 +157,72 @@ public class OrbitCameraState extends BaseAppState {
         System.out.println(getClass().getName() + " Disabled");
         InputMapper inputMapper = GuiGlobals.getInstance().getInputMapper();
         inputMapper.deactivateGroup(GROUP);
+        Node rootNode = ((SimpleApplication)getApplication()).getRootNode();       
+        CursorEventControl.removeListenersFromSpatial(rootNode, focalPointListener);
     }
+ 
+    @Override
+    public void update( float tpf ) {
+    }
+
+    @Override
+    public void postRender() { 
+    }
+ 
+    protected Vector3f getFocalPoint() {
+        if( focalPoint != null ) {
+            return focalPoint;
+        }
+        
+        // Else we will calculate one from the current camera position
+        // and the cursor position
+        Vector2f cursor = getApplication().getInputManager().getCursorPosition();
+        Vector3f near = cam.getWorldCoordinates(cursor, 0);
+        Vector3f far = cam.getWorldCoordinates(cursor, 1);
+        Vector3f dir = far.subtractLocal(near).normalizeLocal();
+        
+        calculatedFocalPoint.set(cam.getLocation());
+        calculatedFocalPoint.addLocal(dir.mult(10));
+        return calculatedFocalPoint;
+    }
+ 
+    private class FocalPointListener extends DefaultCursorListener {
+    
+        protected void setFocalPoint( CursorMotionEvent event ) {
+            if( event.getCollision() != null ) {
+                focalPoint = event.getCollision().getContactPoint(); 
+            } else {
+                focalPoint = null;
+            }
+        }
+    
+        @Override       
+        public void cursorMoved( CursorMotionEvent event, Spatial target, Spatial capture ) {
+            setFocalPoint(event);
+        }
+ 
+        @Override
+        public void cursorExited( CursorMotionEvent event, Spatial target, Spatial capture ) {
+            setFocalPoint(event);
+        } 
+
+        @Override
+        public void cursorEntered( CursorMotionEvent event, Spatial target, Spatial capture ) {
+            setFocalPoint(event);
+        } 
+ 
+        @Override       
+        public void cursorButtonEvent( CursorButtonEvent event, Spatial target, Spatial capture ) {
+            // Have to override this method or the base class will consume the
+            // button event and we don't want that... because then input handler won't 
+            // get it.
+        }
+    } 
     
     private class InputHandler implements AnalogFunctionListener, StateFunctionListener {
+ 
+        private Vector3f fp;
+        private Quaternion flip = new Quaternion().fromAngles(0, FastMath.PI, 0);
  
         public void valueChanged( FunctionId func, InputState value, double tpf ) {
             if( func == F_FAST ) {
@@ -155,16 +231,81 @@ public class OrbitCameraState extends BaseAppState {
                 } else {
                     speed = normalSpeed;
                 }
+            } else if( func == F_ORBIT_ON ) {
+                System.out.println("state change:" + func + "   value:" + value);
+                if( value == InputState.Positive ) {
+                    fp = getFocalPoint();
+                }
             }
         }        
     
         public void valueActive( FunctionId func, double value, double tpf ) {
             System.out.println("Function:" + func + "  value:" + value + "  tpf:" + tpf + "  speed:" + speed);
+ 
+            //System.out.println("Focal point:" + focalPoint);
             
             float amount = (float)(speed * value * tpf); 
             
             Vector3f translate = new Vector3f();
-            if( func == F_MOVE ) {
+            
+            if( func == F_ORBIT_X || func == F_ORBIT_Y ) {
+            
+                // Special processing to both rotate and move around a particular
+                // focal point.
+                //Vector3f fp = getFocalPoint();
+ 
+                System.out.println("fp:" + fp);
+                
+                Vector3f relative = cam.getLocation().subtract(fp);
+                Quaternion rot = cam.getRotation();
+                
+                // Except the camera is looking at the point and we want to
+                // look from the point to the camera... so we'll flip ourselves
+                // around
+                Quaternion orbitRot = rot.mult(flip);
+                
+                float[] angles = orbitRot.toAngles(null);
+System.out.println("angles[" + angles[0] + ", " + angles[1] + ", " + angles[2] + "]");                
+                float yaw = angles[1];
+                float pitch = angles[0];
+                
+                if( func == F_ORBIT_X ) {
+                    yaw += amount;
+                } else if( func == F_ORBIT_Y ) {
+                    pitch -= amount;
+                }
+                Quaternion newRot = new Quaternion().fromAngles(pitch, yaw, angles[2]);
+ 
+                // Find the difference to figure out how much to orbit               
+                Quaternion delta = orbitRot.inverse().mult(newRot);
+                
+                relative = delta.mult(relative);
+                cam.setLocation(fp.add(relative));
+ 
+                // Now flip the rotation back around for the camera view
+                newRot = newRot.mult(flip);
+ 
+                // Rotate the camera
+                cam.setRotation(newRot);
+ 
+                /*               
+                // Something's wrong with the above but let's see if we can partially
+                // salvage it at least
+                Vector3f up = Vector3f.UNIT_Y;
+                Vector3f lookDir = relative.normalize().negateLocal(); 
+                if( Math.abs(lookDir.dot(Vector3f.UNIT_Y)) >= 0.9 ) {
+                    // We'll be nearly looking straight up or straight down
+                    // so we need to calculate a different up vector
+                    up = cam.getLeft().cross(lookDir).normalizeLocal();                   
+                }
+                cam.lookAtDirection(lookDir, up);
+                
+                ....no because the point we clicked is not in the center of the screen.
+                Just going to have to fix the math.
+                
+                */               
+            
+            } else if( func == F_MOVE ) {
                 translate.addLocal(cam.getDirection().mult(amount));
             } else if( func == F_STRAFE ) {
                 translate.addLocal(cam.getLeft().mult(-amount));
