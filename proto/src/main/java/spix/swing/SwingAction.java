3b962f078953bc90aab9866df2cf97c360f0c556
Paul Speed <pspeed42@users.noreply.github.com>
2016-03-22 04:01:20 -0400

    Fixed a kind of bug in the swing action.  Swing will toggle a check box just because it's clicked on... it doesn't _really_ care about the action state.  We generally 100% want to adhere to the action state, though. So if swing tries to toggle the action then we'll set it back again.  The delegate spix.Action will set the toggle as it needs to... which is likely bound to some property. This avoids the UI state being out of sync with the property states.

--- proto/src/main/java/spix/swing/SwingAction.java
+++ proto/src/main/java/spix/swing/SwingAction.java
@@ -1,36 +1,36 @@
 /*
  * $Id$
- * 
+ *
  * Copyright (c) 2016, Simsilica, LLC
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions 
+ * modification, are permitted provided that the following conditions
  * are met:
- * 
- * 1. Redistributions of source code must retain the above copyright 
+ *
+ * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
- * 
- * 2. Redistributions in binary form must reproduce the above copyright 
- *    notice, this list of conditions and the following disclaimer in 
- *    the documentation and/or other materials provided with the 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
  *    distribution.
- * 
- * 3. Neither the name of the copyright holder nor the names of its 
- *    contributors may be used to endorse or promote products derived 
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
  *    from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -57,54 +57,66 @@ public class SwingAction implements javax.swing.Action {
     private Action action;
     private DelegateObserver observer = new DelegateObserver();
     private PropertyChangeSupport dispatcher = new PropertyChangeSupport(this);
-    
+
     public SwingAction( Action action, Spix spix ) {
         this.action = action;
         this.spix = spix;
-        
+
         // Action is thread safe so it's safe to add this here...
         // but we have to have a delegate because we want the events dispatched
         // on the swing thread.  Also we need to rename the properties on the
         // way out.
         this.action.addPropertyChangeListener(observer);
     }
- 
+
     public Action getSpixAction() {
         return action;
     }
-    
+
     public Object getValue( String key ) {
         key = toSpixProperty(key);
         return action.get(key);
     }
-    
+
     public void setEnabled( boolean b ) {
         action.setEnabled(b);
     }
-    
+
     public boolean isEnabled() {
         return action.isEnabled();
     }
-    
+
     public void putValue( String key, Object value ) {
+
+        // A somewhat questionable policy but swing will try to toggle the 'selected'
+        // state regardless of what we do in the action.  This is bad if the toggled
+        // state is actually bound to some property as the checkbox will change even
+        // if the state didn't.  So we filter them out.
+        if( SELECTED_KEY.equals(key) ) {
+            // And not only that... swing will toggle the check even despite our
+            // ignoring it so we have to tell it to switch it back
+            dispatcher.firePropertyChange(SELECTED_KEY, value, action.get(ToggleAction.TOGGLED));
+            return;
+        }
+
         key = toSpixProperty(key);
-        value = toSpixValue(value); 
+        value = toSpixValue(value);
         action.put(key, value);
     }
-    
+
     public void removePropertyChangeListener( PropertyChangeListener listener ) {
         dispatcher.removePropertyChangeListener(listener);
     }
-    
+
     public void addPropertyChangeListener( PropertyChangeListener listener ) {
         dispatcher.addPropertyChangeListener(listener);
     }
- 
+
     public void actionPerformed( ActionEvent e ) {
         spix.runAction(action);
         //action.performAction(spix);
     }
- 
+
     public static String toSwingProperty( String s ) {
         switch( s ) {
             case Action.NAME:
@@ -115,7 +127,7 @@ public class SwingAction implements javax.swing.Action {
                 return s;
         }
     }
-    
+
     public static String toSpixProperty( String s ) {
         switch( s ) {
             case NAME:
@@ -139,31 +151,31 @@ public static final String 	SHORT_DESCRIPTION 	"ShortDescription"
 public static final String 	SMALL_ICON 	"SmallIcon"
 */
     }
- 
+
     public static Object toSwingValue( Object o ) {
-        return o;   
+        return o;
     }
-    
+
     public static Object toSpixValue( Object o ) {
         return o;
-    } 
- 
+    }
+
     protected void firePropertyChange( PropertyChangeEvent event ) {
-System.out.println("SwingAction.firePropertyChange(" + event + ") on thread:" + Thread.currentThread());        
+System.out.println("SwingAction.firePropertyChange(" + event + ") on thread:" + Thread.currentThread());
         String name = toSwingProperty(event.getPropertyName());
-        dispatcher.firePropertyChange(name, event.getOldValue(), event.getNewValue());    
-    } 
-    
+        dispatcher.firePropertyChange(name, event.getOldValue(), event.getNewValue());
+    }
+
     private class DelegateObserver implements PropertyChangeListener {
         public void propertyChange( final PropertyChangeEvent event ) {
-System.out.println("DelegateObserver.propertyChange(" + event + ") on thread:" + Thread.currentThread());        
+System.out.println("DelegateObserver.propertyChange(" + event + ") on thread:" + Thread.currentThread());
             SwingUtilities.invokeLater(new Runnable() {
                 public void run() {
                     firePropertyChange(event);
                 }
             });
         }
-    }    
+    }
 }
 
 
