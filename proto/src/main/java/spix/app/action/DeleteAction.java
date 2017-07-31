2d3340113183a15dca97a09cd8dd4cb0bef648d2
Remy Bouquet <remy.bouquet@gmail.com>
2017-07-31 22:02:04 +0200

    Delete now works properly with multiselection

--- proto/src/main/java/spix/app/action/DeleteAction.java
+++ proto/src/main/java/spix/app/action/DeleteAction.java
@@ -20,38 +20,48 @@ public class DeleteAction extends AbstractAction {
     public DeleteAction(Spix spix) {
         super("Delete", "Delete", "pressed DELETE");
         //we want this menu available only for LightWrappers and spatials, and we don't want users to be able to delete the root of the scene.
-        spix.getBlackboard().bind("main.selection.singleSelect",
-                this, "enabled", Predicates.and(Predicates.or(Predicates.instanceOf(Spatial.class),
-                        Predicates.instanceOf(LightWrapper.class)),
-                        new Predicate<Object>() {
-                            @Override
-                            public boolean apply(Object input) {
-                                return input != spix.getBlackboard().get(SCENE_ROOT);
-                            }
-                        }));
+        spix.getBlackboard().bind("main.selection",
+            this, "enabled", new Predicate<Object>() {
+                @Override
+                public boolean apply(Object input) {
+                    SelectionModel selection = (SelectionModel) input;
+                    Object sceneRoot = spix.getBlackboard().get(SCENE_ROOT);
+                    for (Object o : selection) {
+                        if(o == sceneRoot){
+                            return false;
+                        }
+                        if(Spatial.class.isAssignableFrom(o.getClass()) ||
+                            LightWrapper.class.isAssignableFrom(o.getClass())){
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            });
         setEnabled(false);
     }
 
     @Override
     public void performAction(Spix spix) {
         SelectionModel selection = spix.getBlackboard().get(DefaultConstants.SELECTION_PROPERTY, SelectionModel.class);
-        Object o = selection.getSingleSelection();
-        spix.getService(MessageRequester.class).confirm("Delete Selection", "Do you want to delete the selected element?", new RequestCallback<Boolean>() {
+        spix.getService(MessageRequester.class).confirm("Delete Selection", "Do you want to delete the selected element" + (selection.size()>1?"s?":"?"), new RequestCallback<Boolean>() {
             @Override
             public void done(Boolean confirm) {
                 if (confirm) {
                     UndoManager um = spix.getService(UndoManager.class);
-                    if (o instanceof Spatial) {
-                        Spatial s = (Spatial) o;
-                        Spatial parent = s.getParent();
-                        s.removeFromParent();
-                        um.addEdit(new SpatialRemoveEdit((Node) parent, s));
-                        //select the parent element
-                        selection.setSingleSelection(parent);
-                    } else if (o instanceof LightWrapper) {
-                        LightWrapper lw = (LightWrapper) o;
-                        lw.getParent().removeLight(lw.getLight());
-                        um.addEdit(new LightRemoveEdit(lw.getParent(), lw.getLight()));
+                    for (Object o : selection) {
+                        if (o instanceof Spatial) {
+                            Spatial s = (Spatial) o;
+                            Spatial parent = s.getParent();
+                            s.removeFromParent();
+                            um.addEdit(new SpatialRemoveEdit((Node) parent, s));
+                            //select the parent element
+                            selection.setSingleSelection(parent);
+                        } else if (o instanceof LightWrapper) {
+                            LightWrapper lw = (LightWrapper) o;
+                            lw.getParent().removeLight(lw.getLight());
+                            um.addEdit(new LightRemoveEdit(lw.getParent(), lw.getLight()));
+                        }
                     }
                 }
             }
